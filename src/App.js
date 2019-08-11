f61c6a0c08e36991305d9a5ab8e1116f18df9774
dewarsh <dewarshjoshi505@gmail.com>
2019-08-11 12:17:41 +0530

    Adding navbar switch functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './App.css';
 import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <div className={styles.app}>
-        <Navbar/>
+class App extends Component {
+  state = {
+    activeTab: 0
+  };
+
+  handleTabChange = index => {
+    this.setState({
+      activeTab: index
+    });
+  }
+
+  render() {
+    const { activeTab } = this.state;
+    return (
+      <div className="App">
+        <div className={styles.app}>
+          <Navbar activeTab={activeTab} onTabChange={this.handleTabChange}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import styles from './style.css';
 
-const Navbar = () => {
+const Navbar = props => {
+    const {
+        activeTab,
+        onTabChange
+    } = props;
+
     return (
         <nav className={styles.navbarContainer}>
             <ul className={styles.navbarList}>
-                <li className={styles.navbarListItem}>Items</li>
-                <li className={styles.navbarListItem}>Cart</li>
+                <li className={`${styles.navbarListItem} ${activeTab === 0 ? styles.navbarItemActive : ""}`}>
+                    <a onClick={()=>onTabChange(0)}>Items</a>
+                </li>
+                <li className={`${styles.navbarListItem} ${activeTab === 1 ? styles.navbarItemActive : ""}`}>
+                    <a onClick={()=>onTabChange(1)}>Cart</a>
+                </li>
             </ul>
         </nav>
     );
